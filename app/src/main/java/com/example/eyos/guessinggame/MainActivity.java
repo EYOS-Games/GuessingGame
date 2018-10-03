c502ed4c4b3b59c1388b59beb2187b2a3a832bf5
Eran Semo <eransemo@hotmail.com>
2018-10-03 23:38:42 +0300

    Fixed bugs
    Fixed disabled answer array bug
    get proper indexs of both answer and suggestion array
    dynamic allocate the answer array

--- app/src/main/java/com/example/eyos/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/eyos/guessinggame/MainActivity.java
@@ -7,6 +7,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,16 +21,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ArrayList<EditText> answerArray = new ArrayList<>();
     private ArrayList<EditText> suggetionArray = new ArrayList<>();
 
-    private ArrayList<String> answerArrayStr = new ArrayList<>();
-    private ArrayList<String> suggetionArrayStr = new ArrayList<>();
-
-    private String answer = "136";
+    private String answer = "761";
     private String enteredAnswer  = "";
     
     private String TAG = "0";
 
-    private  int mAvilableAnswerIndex = 0 ;
-    private  int mPressedIndex = 0 ;
+    private LinearLayout mAnswerLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +41,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSeven = findViewById(R.id.editText14);
         mZero = findViewById(R.id.editText13);
 
-        mAnswerOne = findViewById(R.id.editText12);
-        mAnswerTwo = findViewById(R.id.editText11);
-        mAnswerThree = findViewById(R.id.editText10);
+        mAnswerLayout = findViewById(R.id.topLayout);
 
-        answerArray.add(mAnswerOne); answerArray.add(mAnswerTwo); answerArray.add(mAnswerThree);
+        Utilities.createAnswerArray(answer, answerArray, mAnswerLayout, this);
+//        mAnswerOne = findViewById(R.id.editText12);
+//        mAnswerTwo = findViewById(R.id.editText11);
+//        mAnswerThree = findViewById(R.id.editText10);
+
+//        answerArray.add(mAnswerOne); answerArray.add(mAnswerTwo); answerArray.add(mAnswerThree);
         suggetionArray.add(mOne);  suggetionArray.add(mThree);  suggetionArray.add(mSix);  suggetionArray.add(mSeven);  suggetionArray.add(mZero);
     }
 
@@ -56,59 +58,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         EditText et = (EditText)v;
         Object tag = et.getTag();
         String text = et.getText().toString();
-
-
+        EditText selectedSuggestion, selectedAnswer;
+        int fetchIndex, insertIndex;
         //if top layout
         if(tag != null) {
             // if answer is in place
             if (!TextUtils.isEmpty(text)) {
-               // et.setText(""); //remove text
 
-                //enter back to suggetion array
-                int index = -1;
-                for (int i = 0 ; i < suggetionArray.size(); i++)
-                    if (suggetionArray.get(i).getText().toString().equals(text))
-                    {
-                        index = i;
-                        break;
-                    }
-
-                suggetionArray.set(index, et).setTextColor(Color.BLACK);
-                suggetionArray.set(index, et).setEnabled(true);
+                 //insert back to suggetion array
+                insertIndex = Utilities.getEditTextIndex(suggetionArray, text);
+                selectedSuggestion = suggetionArray.get(insertIndex);
+                suggetionArray.set(insertIndex, selectedSuggestion).setTextColor(Color.BLACK);
+                suggetionArray.set(insertIndex, selectedSuggestion).setEnabled(true);
 
 
-                //remove from answer\
-                et.setText("");
-                mAvilableAnswerIndex--;
-                answerArray.set(mAvilableAnswerIndex, et);
+                //remove from answer array
+                fetchIndex = answerArray.indexOf(et);
+                answerArray.set(fetchIndex, et).setText("");
+
+                //remove char from
 
 
             }
         }
         else{
-            mPressedIndex = suggetionArray.indexOf(et);
-            //remove from possible solution
-
-            suggetionArray.set(mPressedIndex, et).setTextColor(Color.GRAY);
-            suggetionArray.set(mPressedIndex, et).setEnabled(false);
-         //   et.setEnabled(false);
-          //  et.setTextColor(Color.GRAY);
 
             //insert to solution
-            if(mAvilableAnswerIndex < answerArray.size() ){
-                answerArray.set(mAvilableAnswerIndex, et).setText(text);
-                mAvilableAnswerIndex++;
-            }
+          //  if(0 < answerArray.size() ){
 
-            if (mAvilableAnswerIndex == answerArray.size()){
+                //insert to answer array
+                insertIndex = Utilities.getEditTextIndex(answerArray, "");
+                selectedAnswer = answerArray.get(insertIndex);
+                answerArray.set(insertIndex, selectedAnswer).setText(text);
+
+                //remove from possible solution
+                fetchIndex = suggetionArray.indexOf(et);
+                suggetionArray.set(fetchIndex, et).setTextColor(Color.GRAY);
+                suggetionArray.set(fetchIndex, et).setEnabled(false);
+
+           // }
+
+            if (Utilities.checkIfAnswerArrayIsFull(answerArray)){
                 for (int i = 0 ; i < answerArray.size(); i++ )
                     enteredAnswer += answerArray.get(i).getText().toString();
                 Log.d(TAG, "enteredAnswer: " + enteredAnswer);
 
                 if (enteredAnswer.equals(answer) )
                     Toast.makeText(this, "good", Toast.LENGTH_LONG).show();
-                else
+                else{
                     Toast.makeText(this, "not good", Toast.LENGTH_LONG).show();
+                    enteredAnswer = "";
+                }
+
             }
         }
 
