4908ee9789c0f6a64f9499d778d6818ffd273b97
Eran Semo <eransemo@hotmail.com>
2018-10-05 15:45:35 +0300

    adding words reading from txt file from assets
    1. adding reading from text file
    2. add helpers #region in utilitis file
    3. add reading word by word funtion ignoring newline attribute
    4. add generic function for reading input file streams.. could be used for getting pictures to ;)
    5.  todo - pick word according to picture

--- app/src/main/java/com/example/eyos/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/eyos/guessinggame/MainActivity.java
@@ -9,19 +9,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-
-
     private ArrayList<EditText> answerArray = new ArrayList<>();
     private ArrayList<EditText> suggetionArray = new ArrayList<>();
 
-    private String answer = "zft";
+    private String answer = "";
     private String enteredAnswer  = "";
     
     private String TAG = "0";
@@ -29,98 +25,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private LinearLayout mAnswerLayout;
     private TableLayout mSuggetionLayout;
 
-    private TableRow bottomFirst, bottmSecond;
-
-
+    private String[] wordsArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        wordsArray = Utilities.readFileWords("words.txt",this);
+        answer = wordsArray[0].toLowerCase().trim(); //should be according to picture number or something
 
         mAnswerLayout = findViewById(R.id.topLayout);
-        mSuggetionLayout  = findViewById(R.id.bottomLayout);
-
-      //  bottomFirst = findViewById(R.id.bottomRow1);
-     //   bottmSecond = findViewById(R.id.bottomRow2);
+        mSuggetionLayout = findViewById(R.id.bottomLayout);
 
         Utilities.createAnswerArray(answer, answerArray, mAnswerLayout, this);
         Utilities.createSuggetionArray(answer, suggetionArray, mSuggetionLayout, this);
-
-
-
-
-
-
     }
 
     @Override
     public void onClick(View v) {
 
-        EditText et = (EditText)v;
+        EditText et = (EditText) v;
         Object tag = et.getTag();
         String text = et.getText().toString();
         EditText selectedSuggestion, selectedAnswer;
         int fetchIndex, insertIndex;
         //if answer array
-        if(tag != null) {
+        if (tag != null) {
             // if answer is in place
             if (!TextUtils.isEmpty(text)) {
 
-                 //insert back to suggetion array
+                //insert back to suggetion array
                 insertIndex = Utilities.getEditTextIndex(suggetionArray, text);
                 selectedSuggestion = suggetionArray.get(insertIndex);
                 suggetionArray.set(insertIndex, selectedSuggestion).setTextColor(Color.BLACK);
                 suggetionArray.set(insertIndex, selectedSuggestion).setEnabled(true);
 
-
                 //remove from answer array
                 fetchIndex = answerArray.indexOf(et);
                 answerArray.set(fetchIndex, et).setText("");
-
-                //remove char from
-
-
             }
         }
         //if suggetion array
-        else{
+        else {
+            //insert to answer array
+            insertIndex = Utilities.getEditTextIndex(answerArray, "");
+            selectedAnswer = answerArray.get(insertIndex);
+            answerArray.set(insertIndex, selectedAnswer).setText(text);
 
-            //insert to solution
-          //  if(0 < answerArray.size() ){
+            //remove from possible solution
+            fetchIndex = suggetionArray.indexOf(et);
+            suggetionArray.set(fetchIndex, et).setTextColor(Color.GRAY);
+            suggetionArray.set(fetchIndex, et).setEnabled(false);
 
-                //insert to answer array
-                insertIndex = Utilities.getEditTextIndex(answerArray, "");
-                selectedAnswer = answerArray.get(insertIndex);
-                answerArray.set(insertIndex, selectedAnswer).setText(text);
-
-                //remove from possible solution
-                fetchIndex = suggetionArray.indexOf(et);
-                suggetionArray.set(fetchIndex, et).setTextColor(Color.GRAY);
-                suggetionArray.set(fetchIndex, et).setEnabled(false);
-
-           // }
-
-            if (Utilities.checkIfAnswerArrayIsFull(answerArray)){
-                for (int i = 0 ; i < answerArray.size(); i++ )
+            if (Utilities.checkIfAnswerArrayIsFull(answerArray)) {
+                for (int i = 0; i < answerArray.size(); i++)
                     enteredAnswer += answerArray.get(i).getText().toString();
                 Log.d(TAG, "enteredAnswer: " + enteredAnswer);
 
-                if (enteredAnswer.equals(answer) )
+                if (enteredAnswer.equals(answer))
                     Toast.makeText(this, "good", Toast.LENGTH_LONG).show();
-                else{
+                else {
                     Toast.makeText(this, "not good", Toast.LENGTH_LONG).show();
                     enteredAnswer = "";
                 }
-
             }
         }
-
-
     }
-
-
 
 }
